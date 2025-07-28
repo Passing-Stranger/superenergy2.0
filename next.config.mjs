4d99f73af7612b86a25d5c80422f2a51cf85a3ea
Passing-Stranger <121397066+Passing-Stranger@users.noreply.github.com>
2025-07-28 15:14:44 +0100

    portet it to next js without images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // Enable optimization for all image formats
+    formats: ['image/webp', 'image/avif', 'image/jpg', "image/png"],
+    // If you need to load images from external domains, add them here
+    // domains: ['example.com'],
+  },
+  // Enable static exports if you want to deploy as a static site
+  // output: 'export',
+  // trailingSlash: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
